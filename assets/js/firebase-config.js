b600f5648c71e7edfd8a46fff889c4fb009fdae7
AlirezaNezami96 <alireza.nezami75@gmail.com>
2026-08-13 12:37:58 +0330

    chore: update Firebase configuration settings and initialization parameters

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -11,12 +11,24 @@
 // session duration, and page views in real-time in your Firebase Console dashboard!
 // ─────────────────────────────────────────────────────────────────────────────
 
-const FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "G-YOUR_MEASUREMENT_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-p3PPWaiJoRHdqhO3sv4_Tt1rmgZwBJc",
+  authDomain: "alireza-portfolio.firebaseapp.com",
+  projectId: "alireza-portfolio",
+  storageBucket: "alireza-portfolio.firebasestorage.app",
+  messagingSenderId: "244392362214",
+  appId: "1:244392362214:web:ab4febdc055288eab7fb27",
+  measurementId: "G-RF4MQ5BV9L"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
